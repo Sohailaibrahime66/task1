887ad356a998b2f8641cbc468294127b993186fb
Sohailaibrahime66 <[200~Sohailaibrahime66~>
2023-03-14 22:50:58 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+for(var i=0 ;i<3 ;i++){
+    var img =document.createElement('img')
+    var text=document.createTextNode('full stack 92')
+    img.setAttribute('src' ,'re1.jfif ')
+    img.append('text')
+    document.body.append(img)
+    // img.style.margin='20px'
+    // img.style.width='30%'
+    // img.style.height='500px'
+    img.style.cssText="margin: 20px ; width: 30%; height: 600px; "
+    document.body.style.backgroundColor='rgb(224, 47, 77)'
+   }
